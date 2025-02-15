d81f4dfa52fd0fc4b55eb3d35bd9c19c7248b143
AleksKosolapov <sashakosolapov1998@yandex.ru>
2025-02-15 18:38:47 +0300

    Исправлены UI-тесты, появление и скрытие алерта

--- MovieQuiz/MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuiz/MovieQuizUITests/MovieQuizUITests.swift
@@ -59,4 +59,45 @@ class MovieQuizUITests: XCTestCase {
        
         XCTAssertNotEqual(firstPosterData, secondPosterData)
     }
+    // Новый тест: проверка появления алерта при завершении игры
+    func testGameFinish() {
+        sleep(2)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(2) // Даем UI время обновиться
+        }
+        
+        print(app.debugDescription) // Проверить структуру элементов в консоли
+
+        let alert = app.alerts.firstMatch // Берем первый найденный алерт
+
+        XCTAssertTrue(alert.waitForExistence(timeout: 10), "Алерт не появился") // Увеличен таймаут ожидания
+        XCTAssertEqual(alert.label, "Этот раунд окончен!", "Неверный заголовок алерта")
+        XCTAssertEqual(alert.buttons.firstMatch.label, "Сыграть ещё раз", "Неверный текст на кнопке алерта")
+    }
+    
+    func testAlertDismiss() {
+        sleep(2)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(2) // Даем UI время обновиться
+        }
+        
+        let alert = app.alerts.firstMatch // Берем первый найденный алерт
+        
+        XCTAssertTrue(alert.waitForExistence(timeout: 10), "Алерт не появился") // Ждем появления алерта
+        
+        alert.buttons.firstMatch.tap() // Закрываем алерт
+        
+        XCTAssertFalse(alert.waitForExistence(timeout: 3), "Алерт не исчез") // Проверяем, что алерт исчез
+
+        sleep(2) // Даем UI время вернуться к игре
+
+        print(app.debugDescription) // Проверяем, какой идентификатор у счетчика
+
+        let indexLabel = app.staticTexts["1/10"] // Ищем конкретное значение счетчика
+
+        XCTAssertTrue(indexLabel.waitForExistence(timeout: 5), "Не найден текстовый элемент индекса")
+        XCTAssertEqual(indexLabel.label, "1/10", "Счётчик не сбросился на 1/10")
+    }
 }
